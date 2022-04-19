c54ddfb4ad43d36dda4a5740f50f403f721dffd7
SavagePastaMan <69145546+SavagePastaMan@users.noreply.github.com>
2022-04-18 20:42:59 -0400

    Create basic hierarchy with link from index to upload_form
    Add 2 new routes for getting the form and submitting the form.
    
    The basic form lets user input new articles. This doesn't yet persist any of the data, though.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,33 @@
 #[macro_use] extern crate rocket;
+use std::path::{Path};
+use rocket::fs::{NamedFile};
+use rocket::form::Form;
+
 
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+async fn index() -> Option<NamedFile> {
+    NamedFile::open(Path::new("static/index.html")).await.ok()
+}
+
+#[derive(FromForm, Debug)]
+struct Article<'r> {
+    title: &'r str,
+    body: &'r str,
+}
+
+#[get("/form")]
+async fn form() -> Option<NamedFile> {
+    NamedFile::open(Path::new("static/upload_form.html")).await.ok()
+}
+
+#[post("/submit", data = "<form>")]
+fn submit(form: Form<Article<'_>>) -> &'static str {
+    println!("{:?}", form.into_inner());
+    "yo"
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build()
+        .mount("/", routes![index, form, submit])
 }
