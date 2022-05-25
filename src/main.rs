0d5889b226be966b454488ce3504c5b3b7480f8e
SavagePastaMan <69145546+SavagePastaMan@users.noreply.github.com>
2022-05-25 10:29:23 -0400

    Compile markdown to html on startup

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,20 @@
 extern crate rocket;
 
 #[macro_use]
+extern crate lazy_static;
+
+#[macro_use]
 extern crate serde_json;
 
 use crate::article::Article;
 use rocket::fs::{relative, FileServer};
 use rocket::http::Status;
 use rocket_dyn_templates::Template;
+use std::io::Result as IoResult;
+use std::path::Path;
 
 mod article;
 mod get_article;
-mod submit_article;
 
 #[get("/")]
 async fn index() -> Result<Template, Status> {
@@ -25,18 +29,29 @@ async fn index() -> Result<Template, Status> {
 
 #[launch]
 fn rocket() -> _ {
+    compile_markdown().unwrap();
+
     rocket::build()
         .mount(
             "/",
-            routes![
-                index,
-                submit_article::form,
-                submit_article::submit,
-                get_article::article_page,
-                get_article::articles
-            ],
+            routes![index, get_article::article_page, get_article::articles],
         )
         .mount("/styles", FileServer::from(relative!("styles")))
         .mount("/fonts", FileServer::from(relative!("fonts")))
         .attach(Template::fairing())
+}
+
+fn compile_markdown() -> IoResult<()> {
+    for entry in std::fs::read_dir("articles/md")? {
+        let path = entry?.path();
+        let html_path = Path::new("articles/json")
+            .join(path.file_name().unwrap())
+            .with_extension("json");
+        if !html_path.exists() {
+            println!("{:?}", path);
+            Article::compile_markdown(path)?
+        }
+    }
+
+    Ok(())
 }
